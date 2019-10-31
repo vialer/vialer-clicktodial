3b3e31bfab4f2fc6b332481ef351504ab2a0859d
dagmar <dagmar.hoogendorp@wearespindle.com>
2019-10-31 16:31:37 +0100

    then instead of await, because of error

--- src/utils/startTrackingUser.mjs
+++ src/utils/startTrackingUser.mjs
@@ -4,11 +4,12 @@ import * as segment from '/lib/segment.mjs';
 const logger = new Logger('startTrackingUser');
 
 export async function startTrackingUser() {
-    const user = await browser.storage.local.get("user");
-    if (Object.keys(user).length !== 0) {
-        let email = user.user.email;
-        await segment.setUserId(email);
-    } else {
-        logger.warn('User not yet logged in, tracking not possible');
-    }
+    browser.storage.local.get("user").then(async(user)=>{
+        if (Object.keys(user).length !== 0) {
+            let email = user.user.email;
+            await segment.setUserId(email);
+        } else {
+            logger.warn('User not yet logged in, tracking not possible');
+        }
+    });
 }
