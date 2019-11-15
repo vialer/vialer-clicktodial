55410f3742c445bfd09839a17cb782be19b645ca
dagmar <dagmar.hoogendorp@wearespindle.com>
2019-11-15 14:16:20 +0100

    remove comments from rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,3 @@
-// import resolve from 'rollup-plugin-node-resolve'
-// import babel from 'rollup-plugin-babel'
-
-// export default [
-//   {
-//     input: 'src/content-entry.js',
-//     output: {
-//       file: 'src/content.js',
-//       format: 'iife',
-//       globals: {
-//         jquery: '$'
-//       }
-//     },
-//     plugins: [
-//       resolve(),
-//     ]
-//   },
-//   {
-//     input: 'src/background-entry.js',
-//     output: {
-//       file: 'src/background.js',
-//       format: 'iife'
-//     },
-//     plugins: [
-//       resolve(),
-//     ]
-//   }
-// ]
-
 import rootImport from 'rollup-plugin-root-import';
 import { terser } from 'rollup-plugin-terser';
 import replace from 'rollup-plugin-replace';
