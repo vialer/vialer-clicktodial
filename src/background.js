4609859eb179eefa0f707333f05d1af0d508d39c
Dagmar <dagmar.hoogendorp@wearespindle.com>
2019-11-22 15:57:29 +0100

    Resolve "Change segment implementation"

--- src/background.js
+++ src/background.js
@@ -1,28 +1,20 @@
-import { clickToDial } from "./lib/data.mjs";
-import { showNotification } from "./lib/notify.mjs";
-import { Logger } from "./lib/logging.mjs";
-import browser from "/vendor/browser-polyfill.js";
+import { clickToDial } from './lib/data.mjs';
+import { showNotification } from './lib/notify.mjs';
+import { Logger } from './lib/logging.mjs';
+import browser from '/vendor/browser-polyfill.js';
+import * as segment from './lib/segment.mjs';
 
-// import 'webextension-polyfill';
-// import * as segment from './lib/segment.mjs';
-// import { startTrackingUser } from './utils/startTrackingUser.mjs';
+const logger = new Logger('background');
 
-// startTrackingUser();
-const logger = new Logger("background");
-
-browser.runtime.onMessage.addListener(async function(
-  request,
-  sender,
-  sendResponse
-) {
+browser.runtime.onMessage.addListener(async function(request, sender, sendResponse) {
   if (request.b_number) {
     clickToDial(request.b_number)
       .then(({ b_number }) => {
         showNotification(`calling ${b_number}`);
+        segment.track.clickedToDial();
       })
       .catch(e => {
         logger.error(e);
       });
-    //TODO notificatie als er error status code terugkomt
   }
 });
