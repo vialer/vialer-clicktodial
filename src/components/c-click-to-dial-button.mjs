bf0c73fcc47962c3efb145d6689bb3855a6bd5dd
Mikolaj Michta <mikolaj.michta@wearespindle.com>
2019-12-04 10:29:49 +0100

    made the icon nicer looking

--- src/components/c-click-to-dial-button.mjs
+++ src/components/c-click-to-dial-button.mjs
@@ -4,12 +4,29 @@ import browser from '/vendor/browser-polyfill.js';
 const logger = new Logger('click-to-dial-button');
 const phoneIconClassName = 'vialer-icon';
 
+const buttonStyle = `
+      -moz-border-radius: 9px;
+      -moz-box-shadow: 0 0 4px 1px rgba(0, 0, 0, 0.2);
+      background-image: url('${browser.runtime.getURL('icons/phone.png')}');
+      background-color: transparent;
+      background-position: center center;
+      background-repeat: no-repeat;
+      cursor: pointer;
+      border-radius: 9px;
+      bottom: -3px;
+      box-shadow: 0 0 4px 1px rgba(0, 0, 0, 0.2);
+      display: inline-block;
+      height: 18px;
+      line-height: 18px;
+      margin: 0 4px;
+      padding: 0;
+      position: relative;
+      width: 18px;
+`;
+
 const template = document.createElement('template');
 template.innerHTML = `
-<button data-selector="click-to-dial-button">
-knop
-<c-icon icon="phone"></c-icon>
-</button>
+<button data-selector="click-to-dial-button" style="${buttonStyle}"></button>
 `;
 
 window.customElements.define(
