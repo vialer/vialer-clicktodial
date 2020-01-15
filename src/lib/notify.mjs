af19408fd79c9343a1ca6f455412ef69dff88ad1
dagmar <dagmar.hoogendorp@wearespindle.com>
2020-01-15 17:45:28 +0100

    put silent option on true

--- src/lib/notify.mjs
+++ src/lib/notify.mjs
@@ -1,7 +1,6 @@
-export function showNotification(title, options) {
-  // solution for annoying sounds
-  // var options = {
-  //   silent: true
-  // }
+export function showNotification(title) {
+  var options = {
+    silent: true
+  };
   let notification = new Notification(title, options);
 }
