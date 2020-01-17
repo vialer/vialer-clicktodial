dda3e710b9e2ca38b79cdfa1606ea9c09e41cf96
dagmar <dagmar.hoogendorp@wearespindle.com>
2020-01-17 15:36:04 +0100

    using debounce to avoid user spam

--- src/background.js
+++ src/background.js
@@ -5,28 +5,28 @@ import { Logger } from './lib/logging.mjs';
 import { translate } from './lib/i18n.mjs';
 import cleanPhoneNumber from './utils/cleanPhoneNumber.mjs';
 import * as segment from './lib/segment.mjs';
+import debounce from './utils/debounce.mjs';
 
 const logger = new Logger('background');
 
 async function doClickToDial(number) {
-  let inCall = (await localStorage.getItem('callid')) !== null;
-  if (!inCall) {
-    clickToDial(number)
-      .then(({ b_number }) => {
-        translate('calling').then(callingMessage => {
-          showNotification(`${callingMessage}: ${b_number}`);
-        });
-        segment.track.clickedToDial();
-      })
-      .catch(e => {
-        logger.error(e);
+  clickToDial(number)
+    .then(({ b_number }) => {
+      translate('calling').then(callingMessage => {
+        showNotification(`${callingMessage}: ${b_number}`);
       });
-  }
+      segment.track.clickedToDial();
+    })
+    .catch(e => {
+      logger.error(e);
+    });
 }
+
+const debouncedClicktoDial = debounce({ fn: doClickToDial, delay: 3000, immediate: true });
 
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.b_number) {
-    doClickToDial(request.b_number);
+    debouncedClicktoDial(request.b_number);
   }
 });
 
@@ -34,7 +34,7 @@ browser.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId === 'select-number') {
     const phoneNumber = cleanPhoneNumber(info.selectionText);
     if (phoneNumber) {
-      doClickToDial(phoneNumber);
+      debouncedClicktoDial(phoneNumber);
     }
   }
 });
