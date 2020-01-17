4451b00580a884be92067263d9de971d313897d2
dagmar <dagmar.hoogendorp@wearespindle.com>
2020-01-17 14:20:22 +0100

    firefox has it's own notification and so does chrome

--- src/lib/notify.mjs
+++ src/lib/notify.mjs
@@ -1,3 +1,12 @@
+import browser from '/vendor/browser-polyfill.js';
+
+const BRAND = '%%BRAND_NAME%%';
+
 export function showNotification(title, options) {
+  browser.notifications.create({
+    title: `${BRAND}`,
+    message: `${title}`,
+    type: 'basic'
+  });
   let notification = new Notification(title, options);
 }
